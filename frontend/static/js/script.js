8e332b2b728a926dcad7eb73b395bd75ec1fba53
William May <wjrm500@gmail.com>
2021-07-29 14:07:09 +0100

    Dynamic styling of league table (standings) according to number of clubs; allowing only even values for "Number of clubs" number input field (for new simulations)

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    styleLeagueTable();
+
     window.iframeHistoryPointer = 0;
     window.iframeHistory = [];
 
@@ -222,3 +224,41 @@ $(document).ready(function() {
         });
     });
 });
+
+function styleLeagueTable() {
+    let leagueTable = $('#league-table')
+    let numClubs = leagueTable.find('tr').length - 1;
+    let numProRel = Math.floor(numClubs / 5);
+
+    // Colour promoted rows
+    let proRows = leagueTable.find(`tr:not(:nth-child(1)):nth-child(-n + ${numProRel + 1})`);
+    proRows.css('backgroundColor', 'lightgreen');
+    proRows.hover(
+        function () {
+            $(this).css('backgroundColor', '#19a519');
+        },
+        function () {
+            $(this).css('backgroundColor', 'lightgreen');
+        }
+    );
+    
+    // Colour relegated rows
+    let relRows = leagueTable.find(`tr:nth-last-child(-n + ${numProRel})`);
+    relRows.css('backgroundColor', 'lightpink');
+    relRows.hover(
+        function () {
+            $(this).css('backgroundColor', '#ff5e76');
+        },
+        function () {
+            $(this).css('backgroundColor', 'lightpink');
+        }
+    );
+
+    // Add dashed lines to visually separate promoted and relegated rows from rest of table
+    leagueTable.find(`tr:nth-child(${numProRel + 1})`).css(
+        'borderBottom', '1px dashed black'
+    );
+    leagueTable.find(`tr:nth-last-child(${numProRel})`).css(
+        'borderTop', '1px dashed black'
+    );
+}
