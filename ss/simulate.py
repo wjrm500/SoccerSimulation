a8cccb15c70a0338c9ecf32b2907c2c913ca435d
William May <wjrm500@gmail.com>
2025-02-11 00:14:00 +0000

    On new simulation, only remove specific stuff from the Redis cache, not everything (as this removes universe pickles which we want to keep / let expire naturally)

--- ss/simulate.py
+++ ss/simulate.py
@@ -9,19 +9,28 @@ import time
 r = redis.Redis(host='redis', port=6379)
 
 def simulate(customConfig, systemId, universeKey):
-    ### Create Universe, taking in input parameters from user
-    universe = Universe(customConfig = customConfig, systemIds = [systemId])
+    # Create Universe based on the input parameters.
+    universe = Universe(customConfig=customConfig, systemIds=[systemId])
     universe.universeKey = universeKey
     daysToTimeTravel = 300
     universe.timeTravel(daysToTimeTravel, r)
+    
+    # Serialise the Universe.
     pickledUniverse = utils.joblibDumps(universe)
+    
+    # Persist the serialised Universe into GridFS.
     db = Database.getInstance()
     cnx = db.cnx.grid_file
     fs = gridfs.GridFS(cnx)
-    fs.put(pickledUniverse, filename = universeKey)
-    if r.exists('email_' + universeKey):
+    fs.put(pickledUniverse, filename=universeKey)
+    
+    # If an email was provided, wait until the Universe is confirmed in GridFS then send an email.
+    if r.exists("email_" + universeKey):
         while not Database.getInstance().universeKeyExists(universeKey):
             time.sleep(5)
-        recipient_address = r.get('email_' + universeKey).decode('utf-8')
+        recipient_address = r.get("email_" + universeKey).decode("utf-8")
         send_email(recipient_address, universeKey)
-    r.flushall()
+    
+    # Remove the data in the Redis cache no longer needed:
+    r.delete("simulation_progress_" + universeKey)
+    r.delete("email_" + universeKey)
